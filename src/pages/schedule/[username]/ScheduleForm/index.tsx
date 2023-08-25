5e6ced2cc42a4587dcd429e72bd5e02a0b15fe3a
christyansegecs <christyansegecs@hotmail.com>
2023-08-25 18:57:12 -0300

    initial commit

---
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -0,0 +1,25 @@
+
+import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
+import { useState } from 'react'
+
+
+export function ScheduleForm() {
+
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
+
+  function handleClearSelectedDateTime() {
+    setSelectedDateTime(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        schedulingDate={selectedDateTime}
+        onCancelConfirmation={handleClearSelectedDateTime}
+      />
+    )
+  }
+
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />
+}
